3de7c2dbab25cee87ca282454018a1af8dd84f86
Nirbhay Gaur <nirbhaygaur.cer18@itbhu.ac.in>
2021-09-02 20:51:10 +0530

    remove facebook login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,13 +13,13 @@ function Login() {
         >
           <GoogleOutlined /> Sign in with Google
         </div>
-        <br /> <br />
+        {/* <br /> <br />
         <div
           className="login-button facebook"
           onClick={() => auth.signInWithRedirect(facebookAuthProvider)}
         >
           <FacebookOutlined /> Sign in with Facebook
-        </div>
+        </div> */}
       </div>
     </div>
   );
